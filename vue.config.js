091cacb633603609cf4ae55d6eb63ea091e9e425
jupengfei <1347237169@qq.com>
2020-03-05 18:16:20 +0800

    🔧 配置vant主题(未完成)

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,30 @@ module.exports = {
       .set('@views', resolve('src/views'))
       .set('@const', resolve('src/const'))
       .set('static', resolve('src/static'))
+    config.module.rule('')
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.less$/,
+          use: [
+            // ...其他 loader 配置
+            {
+              loader: 'less-loader',
+              options: {
+                modifyVars: {
+                  // 直接覆盖变量
+                  'text-color': '#111',
+                  'border-color': '#eee',
+                  // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+                  //'hack': `true; @import "your-less-file-path.less";`
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
   },
 }
