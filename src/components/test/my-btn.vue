1e0e9728e781f4ef5a36f240f7fe7a8cce5205d0
AngryChocobo <1347237169@qq.com>
2020-03-17 23:48:09 +0800

    📦 首页当作测试页

---
+++ src/components/test/my-btn.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="my-btn" @click="handleClick"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log('my-btn created')
+  },
+  mounted() {
+    console.log('my-btn mounted')
+  },
+  destroyed() {
+    console.log('my-btn destroyed')
+  },
+  methods: {
+    handleClick(event) {
+      console.log('组件收到了click事件：', event)
+      this.$emit('clicked', event)
+    },
+  },
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld message="Welcome to Your Vue.js App" />
+    <my-btn @click.native="show">
+      <span>你</span>
+      <span>好</span>
+      <span>吗</span>
+    </my-btn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Mybtn from '@/components/test/my-btn.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    'my-btn': Mybtn,
+  },
+  created() {
+    console.log('Home created')
+  },
+  mounted() {
+    console.log('Home mounted')
+  },
+  destroyed() {
+    console.log('Home destroyed')
+  },
+  methods: {
+    show(v) {
+      console.log('父组件收到了click事件：', v)
+    },
   },
 }
 </script>
