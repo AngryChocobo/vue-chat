b51437adf5004226843c2fa790c67e54a6f9a707
AngryChocobo <1347237169@qq.com>
2020-10-12 14:14:53 +0800

    🚑 git config core.ignorecase false

---
+++ src/views/My.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="my">
+    <my-nav-bar title="我" :left-arrow="false" />
+    <div class="main-info">
+      <UserAvatar :avatar="loggedInUser.avatar" width="64" height="64" />
+      <div class="info-content">
+        <h3>
+          {{ loggedInUser.nickname }}
+        </h3>
+        <p class="username">用户名：{{ loggedInUser.username }}</p>
+      </div>
+    </div>
+    <van-cell @click="goMySettings"> 设置个人资料 </van-cell>
+    <my-tab-bar />
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import UserAvatar from '@/components/user-avatar.vue'
+import MyTabBar from '@components/my-tab-bar.vue'
+import MyNavBar from '@components/my-nav-bar.vue'
+export default {
+  name: 'My',
+  components: {
+    MyTabBar,
+    MyNavBar,
+    UserAvatar,
+  },
+  computed: {
+    ...mapGetters(['loggedInUser']),
+  },
+  methods: {
+    goMySettings() {
+      this.$router.push('my-settings')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.my {
+  .main-info {
+    display: flex;
+    align-items: flex-start;
+    padding: 10px 16px;
+    .info-content {
+      margin-left: 16px;
+      h3 {
+        margin: 0;
+      }
+      p {
+        margin: 0.5em 0;
+        font-size: 14px;
+        color: #888;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="register">
+    <img class="logo" alt="Vue logo" src="../assets/logo.png" />
+    <van-form @submit="onSubmit" style="padding: 0 30px">
+      <van-field
+        v-model="username"
+        name="username"
+        label="用户名"
+        placeholder="用户名"
+        autocomplete="false"
+        :rules="[{required: true, message: '请填写用户名'}]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="password"
+        label="密码"
+        placeholder="密码"
+        autocomplete="false"
+        :rules="[{required: true, message: '请填写密码'}]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="primary" native-type="submit">
+          注册
+        </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import {REGISTER} from '@store/types/action-types.js'
+
+export default {
+  name: 'Register',
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      const {username, password} = values
+      this.$store.dispatch(REGISTER, {
+        username,
+        password,
+      })
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.register {
+  position: absolute;
+  width: 100%;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  // background-image: url('../assets/timg.jpeg');
+  // background-repeat: no-repeat;
+  // background-size: 100% 100%;
+  .logo {
+    display: block;
+    width: 60%;
+    margin: 0 auto;
+  }
+  .van-cell {
+    background: transparent;
+  }
+  /deep/ .van-field__label {
+    color: black;
+  }
+}
+</style>
