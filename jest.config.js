9757a0e04768c837da3fb8da391d1d86d2ad74de
jupengfei <1347237169@qq.com>
2020-12-18 10:01:30 +0800

    ✨ cypress jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+
+  transform: {
+    '^.+\\.vue$': 'vue-jest'
+  }
 }
